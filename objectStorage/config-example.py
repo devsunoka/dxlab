823b140139d17f8bd13aaf822fb67d95054457e9
sunoka <y-shinooka@netone.co.jp>
2023-06-28 21:09:22 +0900

    add objectStorage codes

---
+++ objectStorage/config-example.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : 'XXXXX@ecstestdrive.emc.com',
+    'ecs_secret_key' : 'O6rI.....',
+    'ecs_bucket_name' : 'photos'
+}
